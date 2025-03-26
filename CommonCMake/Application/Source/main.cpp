99f106d52fbe938890629cf31a3472eedaedc35c
DragonheartLX <dragonheartlx@outlook.com>
2025-03-26 19:33:14 +0800

    update

--- CommonCMake/Application/Source/main.cpp
+++ CommonCMake/Application/Source/main.cpp
@@ -1,4 +1,11 @@
-#include "header.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <header.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) 
+{
+    glViewport(0, 0, width, height);
+}
 
 int main(int argc, char** argv) 
 {
--- CommonCMake/include/header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-void print();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
